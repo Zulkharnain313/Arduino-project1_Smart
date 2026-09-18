1cfee365b9266cf73d8df6ffee2493661ddd00ff
Zul Kharnain Pasha A <zulkharnain835@gmail.com>
2026-09-18 11:53:58 +0530

    update project1_Smart

--- project1_Smart.ino
+++ project1_Smart.ino
@@ -534,5 +534,11 @@ void loop()
   sendCameraPicture(); 
   Serial.printf("SPIRam Total heap %d, SPIRam Free Heap %d\n", ESP.getPsramSize(), ESP.getFreePsram());
 }
- 
+ void loop() 
+{
+  wsCamera.cleanupClients(); 
+  wsCarInput.cleanupClients(); 
+  sendCameraPicture(); 
+  Serial.printf("SPIRam Total heap %d, SPIRam Free Heap %d\n", ESP.getPsramSize(), ESP.getFreePsram());
+}
 
